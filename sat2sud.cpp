df9fced257a5ffbd3d78c11872d405489268c7fb
z834li <z834li@uwaterloo.ca>
2022-11-28 07:11:08 -0500

    Added code documentation

--- sat2sud.cpp
+++ sat2sud.cpp
@@ -4,18 +4,31 @@
 
 using namespace std;
 
-int main(){
+int main(void){
     string temp;
+
+    // Get the first line, it's either SAT or UNSAT.
+    // If the first line is UNSAT, then there is no solution.
     getline(cin,temp);
     if(temp.find("UN") != string::npos) return 0;
+
+    // Get the second line which is the truth assignment of
+    // propositional variables.
     getline(cin,temp);
+
     stringstream inp(temp);
     int val;
     int count = 0;
+
     while(inp >>val){
+        // If the variable is assigned to true, the variable's number
+        // indicates the which value the sudoku entry should be. 
+        // Do a mod operation and print that value to output.
         if(val > 0){
             cout<< (val+8)%9+1;
             count++;
+            // Every 3 entries, print a space.
+            // Every 9 entries, print a newline.
             if(count%3 ==0) cout <<" ";
             if(count == 9){
                 cout<<endl;
@@ -23,4 +36,6 @@ int main(){
             }
         }
     }
+
+    return 0;
 }
--- sud2sat.cpp
+++ sud2sat.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 
 using namespace std;
-int help(int a,int b,int c){
-  if(a > 8 | b > 8) cerr<<"What the hell"<<endl;
- // cout<<"[("<<a<<","<<b<<" )"<<"is "<<c<<" ]";
-  return a*9*9+b*9+c;
+
+/* 
+ * Helper function to find the associated propositional variable s_{xyz}
+ * for when the entry in row x column y is assigned value z.
+ * 
+ * The number can be represented in base-9.
+ */
+int help(int x,int y,int z){
+  if(x > 8 | y > 8) cerr<<"Wrong values"<<endl;
+  return x*9*9+y*9+z;
 }
+
 int main(){
 	char a;
 	int puzzle [9][9];
 	int tot = 0;
+
+	// Use a 9*9 2D array for the puzzle input.
+	// Keep track of each pre-assigned entry and 
+	// calculate the total number of pre-assigned entries
 	for(int i = 0; i < 9;++i){
 		for(int k =0;k < 9;++k){
 			cin >> a;
@@ -20,84 +31,89 @@ int main(){
 			else puzzle[i][k] = 0;
 		}
 	}
-	//trouble shooting
-	// int reference[730];
-	// for(int i = 0;i < 730;++i) reference[i] = 0;
-	//
+	
+	// Look for pre-assigned non-zero entries and translate
+	// them to unit clauses. 
+	// Add to 8829 the total number of pre-assigned entries.
 	cout<<"p cnf "<<9*9*9<<" "<<tot+8829<<endl;
 	for(int i = 0; i < 9;++i){
 		for(int k =0;k < 9;++k){
 		  if(puzzle[i][k]){
 			cout<<help(i,k,puzzle[i][k]) <<" 0"<<endl;
-			//reference[help(i,k,puzzle[i][k])] = 1;
 		  }
 		}
 	}
-	// One number in each entry
 
+	// Add the clauses that represent the condition that
+	// there must be at least one number in each entry to output.
 	for(int x=0;x < 9;++x){
 	  for(int y = 0;y < 9;++y){
-		//int tester = 0;	//trouble shooting
+		//int tester = 0;	// DEBUG
 	    for(int z = 1;z <10;++z){
 	      cout<<help(x,y,z)<<' ';
-		//  if(reference[help(x,y,z)]) tester = 1;	//trouble shooting
+		// if(reference[help(x,y,z)]) tester = 1;	// DEBUG
 	    }
-		//if(tester == 0) cerr<<"Fucked up ENTRY"<<x<<y<<endl;	//trouble shooting
 	    cout<<" 0"<<endl;
 	  }
 	}
-	//for each number appears at most once in each row
-       	for(int x=0;x < 9;++x){
+
+	// Each number appears at most once in each row. 
+    for(int x=0;x < 9;++x){
 	    for(int y = 0;y < 9;++y){
-	      for(int z = 1;z <10;++z){
-		for(int i = x+1;i < 9 ;++i){
-		  cout<<"-"<<help(x,y,z)<<" -"<<help(i,y,z)<<" 0"<<endl;
-		//  if(reference[help(x,y,z)] && reference[help(i,y,z)] ) cout<<"Fucked up ROW " <<x<<y<<i<<endl;//trouble shooting
+	    	for(int z = 1;z <10;++z){
+				for(int i = x+1;i < 9 ;++i){
+					// (¬s_{xyz} ∨ ¬s_{iyz})
+		  			cout<<"-"<<help(x,y,z)<<" -"<<help(i,y,z)<<" 0"<<endl;
+					// if(reference[help(x,y,z)] && reference[help(i,y,z)] ) cout<<"Fucked up ROW " <<x<<y<<i<<endl;// DEBUG
+				}
+	      	}
 		}
-	      }
-	    }
 	}
+
+	// Each number appears at most once in each column.
 	for(int x=0;x < 9;++x){
 	    for(int y = 0;y < 9;++y){
 	      for(int z = 1;z <10;++z){
-		for(int i = y+1;i < 9 ;++i){
-		  cout<<"-"<<help(x,y,z)<<" -"<<help(x,i,z)<<" 0"<<endl;
-		 // 		  if(reference[help(x,y,z)] && reference[help(x,i,z)] ) cout<<"Fucked up Col " <<x<<y<<i<<endl;//trouble shooting
-		}
-	      }
+			for(int i = y+1;i < 9 ;++i){
+		  	  cout<<"-"<<help(x,y,z)<<" -"<<help(x,i,z)<<" 0"<<endl;
+		 	  // if(reference[help(x,y,z)] && reference[help(x,i,z)] ) cout<<"Fucked up Col " <<x<<y<<i<<endl;// DEBUG
+		  }
 	    }
+	  }
 	}
+
+	// Each number appears at most once in each 3x3 sub-grid - part 1:
 	for(int z = 1;z < 10;++z){
 		for(int i = 0; i < 3;++i){
 			for(int j = 0; j < 3;++j){
 	  			for(int x =0;x < 3;++x){
 	    			for(int y = 0; y < 3;++y){
 						  for(int k= y+1;k < 3;++k){
-		    cout<<"-"<<help(3*i+x,3*j+y,z)<<" -"<<help(3*i+x,3*j+k,z)<<" 0"<<endl;
-		//	if(reference[help(3*i+x,3*j+y,z)] && reference[help(3*i+x,3*j+k,z)] ) cout<<"Fucked up 3*3 " <<x<<y<<i<<j<<k<<endl;//trouble shooting
-		  }
-		}  
-	      }
-	    }
-	  }
+		    				cout<<"-"<<help(3*i+x,3*j+y,z)<<" -"<<help(3*i+x,3*j+k,z)<<" 0"<<endl;
+							// if(reference[help(3*i+x,3*j+y,z)] && reference[help(3*i+x,3*j+k,z)] ) cout<<"Fucked up 3*3 " <<x<<y<<i<<j<<k<<endl;// DEBUG
+		  				  }
+					}  
+	      		}
+	    	}
+	  	}
 	}
+
+	// Each number appears at most once in each 3x3 sub-grid - part 2:
 	for(int z = 1;z < 10;++z){
 		for(int i = 0; i < 3;++i){
 			for(int j = 0; j < 3;++j){
 	  			for(int x =0;x < 3;++x){
 	    			for(int y = 0; y < 3;++y){
-
 		  				for(int k= x+1;k < 3;++k){
 		    				for(int l = 0;l<3;++l){
-						//		if(reference[help(3*i+x,3*j+y,z)] && reference[help(3*i+k,3*j+l,z)] ) cout<<"Fucked up 3*3 " <<x<<y<<i<<j<<k<<" ";//trouble shooting
+								// if(reference[help(3*i+x,3*j+y,z)] && reference[help(3*i+k,3*j+l,z)] ) cout<<"Fucked up 3*3 " <<x<<y<<i<<j<<k<<" ";// DEBUG
 		      					cout<<"-"<<help(3*i+x,3*j+y,z)<<" -"<<help(3*i+k,3*j+l,z)<<" 0"<<endl;
-			  
-		    }
-		  }
-		}  
-	      }
-	    }
-	  }
+		    				}
+		  				}
+					}  
+	      		}
+	    	}
+	  	}
 	}
-	
+	// The above condition clauses should suffice for minimal encoding.
 }
